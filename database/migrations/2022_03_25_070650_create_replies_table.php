0618e350e2267bc0da88ded3115cc86d0e5ed0fc
Aung Myat Moe <aungmyatmoe834@gmail.com>
2022-03-25 13:39:47 +0630

    Reply  table design

---
+++ database/migrations/2022_03_25_070650_create_replies_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration {
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('replies', function (Blueprint $table) {
+            $table->id();
+            $table->text('body');
+
+            // foreign key
+            $table->unsignedBigInteger('user_id');
+            $table->unsignedBigInteger('question_id');
+            // delete comment on deleting of question
+            $table->foreignId('question_id')->constrained()->cascadeOnDelete();
+
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('replies');
+    }
+};
