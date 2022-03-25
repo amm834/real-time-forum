e48c6b4a50ab9696988976457fc45585a00f8b04
Aung Myat Moe <aungmyatmoe834@gmail.com>
2022-03-25 14:42:06 +0630

    Seeding data with seeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -2,7 +2,10 @@
 
 namespace Database\Seeders;
 
-use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use App\Models\Category;
+use App\Models\Question;
+use App\Models\Reply;
+use App\Models\User;
 use Illuminate\Database\Seeder;
 
 class DatabaseSeeder extends Seeder
@@ -14,6 +17,12 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        // \App\Models\User::factory(10)->create();
+        User::factory(10)->create();
+        Category::factory(5)->create();
+        Question::factory(10)->create();
+        // hasLikes is magic method
+        Reply::factory(50)
+            ->hasLikes(10)
+            ->create();
     }
 }
