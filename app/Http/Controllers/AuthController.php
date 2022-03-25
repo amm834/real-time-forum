1daccd1b5c6811e3fe72f8e7fe91112780d1eeaa
Aung Myat Moe <aungmyatmoe834@gmail.com>
2022-03-25 22:59:16 +0630

    JsonWebToken with sanctum configuration

---
+++ app/Http/Controllers/AuthController.php
@@ -0,0 +1,51 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\User;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Hash;
+use Symfony\Component\HttpFoundation\Response;
+
+class AuthController extends Controller
+{
+    public function __construct()
+    {
+        $this->middleware('auth:sanctum')->except(['register', 'login']);
+    }
+
+    public function register(Request $request)
+    {
+        $data = $request->validate([
+            'name' => 'required',
+            'email' => 'required|unique:users',
+            'password' => 'required|min:6|max:15',
+        ]);
+
+        $data['password'] = Hash::make($data['password']);
+        $user = User::create($data);
+        return response($user, Response::HTTP_CREATED);
+    }
+
+    public function login(Request $request)
+    {
+        $credentials = $request->validate([
+            'email' => 'required',
+            'password' => 'required',
+        ]);
+
+        if (auth()->attempt($credentials)) {
+            $token = auth()->user()->createToken('access_token')->plainTextToken;
+            return response()->json([
+                'access_token' => $token
+            ]);
+        }
+    }
+
+    public function logout()
+    {
+        // Revoke all tokens...
+        auth()->user()->tokens()->delete();
+        return response()->noContent();
+    }
+}
--- routes/api.php
+++ routes/api.php
@@ -1,16 +1,16 @@
 <?php
 
+use App\Http\Controllers\AuthController;
 use App\Http\Controllers\CategoryController;
 use App\Http\Controllers\LikeController;
 use App\Http\Controllers\QuestionController;
 use App\Http\Controllers\ReplyController;
-use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
 
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-    return $request->user();
-});
+Route::post('/auth/register', [AuthController::class, 'register']);
+Route::post('/auth/login', [AuthController::class, 'login']);
+Route::post('/auth/logout', [AuthController::class, 'logout']);
 
 Route::apiResource('/questions', QuestionController::class);
 Route::apiResource('/categories', CategoryController::class);
